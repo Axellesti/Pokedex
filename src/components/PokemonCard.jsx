144557da1b218ba8be817563daf8ee00064ee6d4
Axellesti <sartori.axelle18@gmail.com>
2023-12-12 17:31:57 +0100

    challenge prop-types

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+
+function PokemonCard(props) {
+    console.log(props);
+
+    return (
+        <figure>
+            {props.pokemon[0].imgSrc ? (
+                <img src={props.pokemon[0].imgSrc} alt={props.pokemon[0].name} />
+            ) : (
+                <p>???</p>
+            )}
+            <figcaption>{props.pokemon[0].name}</figcaption>
+        </figure>
+    );
+  }
+
+
+  PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+    }).isRequired,
+  };
+  
+  
+
+  export default PokemonCard;
+
+
