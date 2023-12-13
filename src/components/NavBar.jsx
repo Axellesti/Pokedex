2c5b1fc8b1a917e68cb4977373ba33f05ca5db5c
Axellesti <sartori.axelle18@gmail.com>
2023-12-13 15:47:16 +0100

    challenge partager un state

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+function NavBar(props) {
+    const {previousClick, nextClick} = props;
+
+    return(
+    <div>
+        <button onClick={previousClick}>Précédent</button>
+        <button onClick={nextClick}>Suivant</button>
+    </div>
+    )
+}
+
+
+export default NavBar
